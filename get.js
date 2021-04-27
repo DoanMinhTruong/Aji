0da5493d440d8ba4af4673d10307c97a9a312fab
unknown <meir.truong.2309@gmail.com>
2021-04-27 19:38:56 +0700

    first commit

---
+++ get.js
@@ -0,0 +1,10 @@
+var voucher = `DM1208`;
+// $(document).ready(function(){
+//     $.ajax({
+//         type: "GET",
+//         url: "http://127.0.0.1:8000/api/code/",
+//         success:    function(data){
+//             voucher = data[1].code
+//         }
+//       });
+// })
